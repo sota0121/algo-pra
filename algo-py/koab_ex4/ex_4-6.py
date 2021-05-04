50a93e58af8959b0c9b2b446e10b820075ebbe30
sota_masuda <sota.m777121@gmail.com>
2021-05-05 08:59:31 +0900

    add ans(bad)

--- algo-py/koab_ex4/ex_4-6.py
+++ algo-py/koab_ex4/ex_4-6.py
@@ -7,6 +7,10 @@ import sys
 from typing import List
 
 
+calc_cnt = 0
+memo = None
+
+
 def partial_sum(i: int, W: int, numbers: List[int]) -> bool:
     # show process
     global calc_cnt
@@ -28,13 +32,52 @@ def partial_sum(i: int, W: int, numbers: List[int]) -> bool:
         return True
     return False
 
+# 解答（メモ化したもの）
+# > O(NW)なので、N個の要素とW個の要素の組み合わせの結果を配列に入れるのかな？
+# >> いやその場合だと計算量の削減につながらない。W=wについてTrue/Falseかわかればいいのだから、Wについて結果を保持しておけばいい
+def partial_sum_memorized(i: int, W: int, numbers: List[int]) -> bool:
+    # すでに計算しているならば計算しない
+    global memo
+    if memo[i][W] is not None:
+        return memo[i][W]
+    
+    # show process
+    global calc_cnt
+    print('call i={0}, W={1}, calc:{2}'.format(i, W, calc_cnt))
+    calc_cnt += 1
+    
+    # base case
+    if i == 0:
+        if W == 0:
+            memo[i][W] = True
+            return True
+        else:
+            memo[i][W] = False
+            return False
+    
+    # a[i-1] を選ばない場合
+    if partial_sum_memorized(i - 1, W, numbers):
+        memo[i-1][W] = True
+        return True
+    # a[i-1] を選ぶ場合
+    if partial_sum_memorized(i - 1, W - numbers[i - 1], numbers):
+        memo[i-1][W] = True
+        return True
+    memo[i][W] = False
+    return False
+
+
 if __name__ == "__main__":
-    calc_cnt = 0
-    numbers = list(map(int, input("numbers(sep space) >> ").split()))
+    #numbers = list(map(int, input("numbers(sep space) >> ").split()))
+    numbers = [i for i in range(1000)]
     N = len(numbers)
     W = int(input('W? >>'))
     
-    if partial_sum(N, W, numbers):
+    # init memo 2d array
+    memo = [[None for _ in range(W+1)] for _ in range(N+1)] # row=N+1, col=W+1
+    
+    if partial_sum_memorized(N, W, numbers):
         print('Yes')
     else:
         print('No')
+    print('N=', N, ' W=', W)
